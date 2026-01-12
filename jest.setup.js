0ff3942b692087b0e4e6ac688a95d3e5597f7fd3
chacha <chayeonggeon@gmail.com>
2026-01-12 19:57:00 +0900

    [#5] Setting: jest config, setup 파일 설정

---
+++ jest.setup.js
@@ -0,0 +1,11 @@
+import "react-native-gesture-handler/jestSetup";
+
+// expo-secure-store 모킹
+jest.mock("expo-secure-store", () => ({
+  getItemAsync: jest.fn(),
+  setItemAsync: jest.fn(),
+  deleteItemAsync: jest.fn(),
+}));
+
+// Mock tokenStorage
+jest.mock("@/lib/tokenStorage/tokenStorage");
