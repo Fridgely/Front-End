9996c4abe9ce4defbfff047596b13227f7cbab4f
Yeonggeon Cha <144249211+cha0cha@users.noreply.github.com>
2026-01-23 19:30:43 +0900

    Feat: 스플래시 스크린 구현 (#10)
    * [#9] File: 스플래시 스크린 이미지 파일 추가
    
    * [#9] Feat: 스플래시 스크린 구현
    
    * [#9] Feat: 스플래시 스크린 에러 핸들링 추가

---
+++ src/components/AnimatedSplashScreen.tsx
@@ -0,0 +1,75 @@
+import React, { useEffect } from "react";
+import { Dimensions, Image, StyleSheet, View } from "react-native";
+import Animated, {
+  Easing,
+  useAnimatedStyle,
+  useSharedValue,
+  withDelay,
+  withTiming,
+} from "react-native-reanimated";
+
+const { width } = Dimensions.get("window");
+
+export default function AnimatedSplashScreen({
+  onAnimationFinish,
+}: {
+  onAnimationFinish: () => void;
+}) {
+  const fadeAnim = useSharedValue(1);
+
+  useEffect(() => {
+    fadeAnim.value = withDelay(
+      500,
+      withTiming(0, {
+        duration: 600,
+        easing: Easing.bezier(0.7, 0, 0.84, 0),
+      }),
+    );
+
+    const timer = setTimeout(() => {
+      onAnimationFinish();
+    }, 1500);
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  const animatedStyle = useAnimatedStyle(() => ({
+    opacity: fadeAnim.value,
+  }));
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.centered}>
+        <Image
+          source={require("../assets/images/open_fridge.png")}
+          style={[
+            styles.logo,
+            { transform: [{ translateX: -2 }, { translateY: 1.3 }] },
+          ]}
+          resizeMode="contain"
+        />
+      </View>
+
+      <Animated.View
+        style={[StyleSheet.absoluteFill, styles.centered, animatedStyle]}
+      >
+        <Image
+          source={require("../assets/images/closed_fridge.png")}
+          style={styles.logo}
+          resizeMode="contain"
+        />
+      </Animated.View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#E0F6EA",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  centered: { justifyContent: "center", alignItems: "center" },
+  logo: { width: width * 0.8, height: width * 0.8 },
+});
