3ed8309f74d25d267680ce209dded5515b19f0cc
Yeonggeon Cha <144249211+cha0cha@users.noreply.github.com>
2026-01-30 21:34:36 +0900

    Refactor: 디자인 토큰 재정의 (#16)

--- src/shared/theme/themes.ts
+++ src/shared/theme/themes.ts
@@ -1,27 +1,29 @@
 import { tokens } from "./tokens";
 
 export const lightTheme = {
-  background: tokens.color.white,
+  background: tokens.color.background,
   color: tokens.color.mainText,
 
   primary: tokens.color.primary,
-  primaryText: tokens.color.white,
+  primaryText: tokens.color.mainText,
+  secondary: tokens.color.secondary,
 
   active: tokens.color.primary,
-  inactive: tokens.color.gray10,
+  inactive: tokens.color.gray,
 
   borderColor: tokens.color.gray3,
 };
 
 export const darkTheme = {
-  background: tokens.color.mainText,
-  color: tokens.color.gray1,
+  background: tokens.color.backgroundDark,
+  color: tokens.color.mainTextDark,
 
-  primary: tokens.color.primary,
-  primaryText: tokens.color.white,
+  primary: tokens.color.primaryDark,
+  primaryText: tokens.color.mainTextDark,
+  secondary: tokens.color.secondaryDark,
 
-  active: tokens.color.primary,
-  inactive: tokens.color.gray10,
+  active: tokens.color.primaryDark,
+  inactive: tokens.color.grayDark,
 
-  borderColor: tokens.color.outline,
+  borderColor: tokens.color.grayDark,
 };
