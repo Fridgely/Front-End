fd03b8ad53e03b89f211306211721da204fe5b43
chacha <chayeonggeon@gmail.com>
2026-03-10 20:34:05 +0900

    [#39] Feat: 냉장고 멤버 불러오기 기능 구현

---
+++ src/features/fridge-management/apis/fridge-member.types.ts
@@ -0,0 +1,8 @@
+import { FridgeMember } from "@/shared/types/fridge";
+
+interface FridgeMemberResponse {
+  result: string;
+  data: FridgeMember[];
+}
+
+export { FridgeMemberResponse };
--- src/shared/constants/queryKeys.ts
+++ src/shared/constants/queryKeys.ts
@@ -3,6 +3,8 @@ const QUERY_KEYS = {
     all: ["fridges"] as const,
     list: () => [...QUERY_KEYS.fridge.all, "list"] as const,
     detail: (id: number) => [...QUERY_KEYS.fridge.all, "detail", id] as const,
+    members: (id: number) =>
+      [...QUERY_KEYS.fridge.detail(id), "members"] as const,
     // 카테고리는 냉장고에 종속적이므로 냉장고 상세 키에 포함시킴
     categories: (id: number) =>
       [...QUERY_KEYS.fridge.detail(id), "categories"] as const,
