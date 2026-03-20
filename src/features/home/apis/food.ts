2edf8baed489730478981ff96ad570ee5fc5e026
chacha <chayeonggeon@gmail.com>
2026-03-20 17:12:12 +0900

    [#51] Feat: 냉장고 별 식품 불러오기 기능 구현

--- src/features/home/apis/food.ts
+++ src/features/home/apis/food.ts
@@ -1,8 +1,22 @@
 import ApiBuilder from "@/shared/apis/builder/ApiBuilder";
-import type { FoodStatusResponse } from "./food.types";
+import type {
+  FoodStatusResponse,
+  FridgeFoodsCursorRequest,
+  FridgeFoodsResponseRaw,
+} from "./food.types";
 
 const getFoodStatusApi = ApiBuilder.create<void, FoodStatusResponse>(
   "/api/v1/foods/status",
 ).setMethod("GET");
 
-export { getFoodStatusApi };
+const getRefrigeratorFoodsApi = (
+  refrigeratorId: number,
+  cursorRequest: FridgeFoodsCursorRequest,
+) =>
+  ApiBuilder.create<void, FridgeFoodsResponseRaw>(
+    `/api/v1/refrigerators/${refrigeratorId}/foods`,
+  )
+    .setMethod("GET")
+    .setParams(cursorRequest);
+
+export { getFoodStatusApi, getRefrigeratorFoodsApi };
