f71c7e146365c68d295dda3a17bc79d51b0b957c
chacha <chayeonggeon@gmail.com>
2026-03-22 22:46:05 +0900

    [#53] Feat: 전체 식품 쿼리 및 냉장고별 식품 쿼리에 정규화 유틸 함수 적용

--- src/features/home/hooks/queries/useAllFoodStatusQuery.ts
+++ src/features/home/hooks/queries/useAllFoodStatusQuery.ts
@@ -1,10 +1,22 @@
 import { useApiQuery } from "@/shared/apis/builder/ApiBuilder";
 import { QUERY_KEYS } from "@/shared/constants/queryKeys";
 import { getFoodStatusApi } from "../../apis/food";
+import { FoodStatusResponse } from "../../apis/food.types";
+import { normalizeFoodItem } from "../../utils/normalizeFoodItem";
 
 const useAllFoodStatusQuery = (enabled = true) => {
   return useApiQuery(getFoodStatusApi, QUERY_KEYS.food.statusAll(), {
     enabled,
+    select: (response: FoodStatusResponse) => ({
+      ...response,
+      data: {
+        ...response.data,
+        black: response.data.black.map((food) => normalizeFoodItem(food)),
+        red: response.data.red.map((food) => normalizeFoodItem(food)),
+        yellow: response.data.yellow.map((food) => normalizeFoodItem(food)),
+        green: response.data.green.map((food) => normalizeFoodItem(food)),
+      },
+    }),
   });
 };
 
