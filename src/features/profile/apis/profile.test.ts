c592c5c74fd2d89e46db7f47b0c831c7cbfa5ebf
chacha <chayeonggeon@gmail.com>
2026-03-31 04:04:15 +0900

    [#67] Test: 프로필 사진 업로드/수정 기능 구현

--- src/features/profile/apis/profile.test.ts
+++ src/features/profile/apis/profile.test.ts
@@ -1,4 +1,8 @@
-import { getMemberProfileApi, logoutApi } from "./profile";
+import {
+  getMemberProfileApi,
+  logoutApi,
+  updateMemberProfileImageApi,
+} from "./profile";
 
 jest.mock("../../../shared/lib/tokenStorage/tokenStorage");
 
@@ -13,5 +17,11 @@ describe("마이페이지 API 테스트", () => {
     const api = getMemberProfileApi as any;
     expect(api.endpoint).toBe("/api/v1/members/me");
     expect(api.method).toBe("GET");
+  });
+
+  it("updateMemberProfileImageApi는 올바른 URL과 메소드로 설정되어야 한다", () => {
+    const api = updateMemberProfileImageApi as any;
+    expect(api.endpoint).toBe("/api/v1/members/me/profile-image");
+    expect(api.method).toBe("PATCH");
   });
 });
