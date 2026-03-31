1f2ff3e563874cdbf3ff1787acfa2ccd5f29b1c4
chacha <chayeonggeon@gmail.com>
2026-03-31 10:01:16 +0900

    [#67] Chore: 테스트 폴더로 파일 이동

--- src/features/profile/__tests__/apis/profile.test.ts
+++ src/features/profile/__tests__/apis/profile.test.ts
@@ -2,9 +2,9 @@ import {
   getMemberProfileApi,
   logoutApi,
   updateMemberProfileImageApi,
-} from "./profile";
+} from "../../apis/profile";
 
-jest.mock("../../../shared/lib/tokenStorage/tokenStorage");
+jest.mock("@/shared/lib/tokenStorage/tokenStorage");
 
 describe("마이페이지 API 테스트", () => {
   it("logoutApi는 올바른 URL과 메소드로 설정되어야 한다", () => {
