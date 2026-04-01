9988bc5dcba81d7fd6b001705208bb6535387286
Yeonggeon Cha <144249211+cha0cha@users.noreply.github.com>
2026-04-01 23:19:37 +0900

    Fix: 앱 초기 진입시 홈 냉장고/식품 쿼리 enabled 조건에 로그인 상태 반영 (#74)

--- src/features/fridge-management/hooks/queries/useFridgeListQuery.tsx
+++ src/features/fridge-management/hooks/queries/useFridgeListQuery.tsx
@@ -1,9 +1,14 @@
+import { useIsLoggedIn } from "@/features/auth/store/useAuthStore";
 import { useApiQuery } from "@/shared/apis/builder/ApiBuilder";
 import { QUERY_KEYS } from "@/shared/constants/queryKeys";
 import { getFridgeListApi } from "../../apis/fridge-management";
 
 const useFridgeListQuery = () => {
-  return useApiQuery(getFridgeListApi(), QUERY_KEYS.fridge.list());
+  const isLoggedIn = useIsLoggedIn();
+
+  return useApiQuery(getFridgeListApi(), QUERY_KEYS.fridge.list(), {
+    enabled: isLoggedIn,
+  });
 };
 
 export { useFridgeListQuery };
