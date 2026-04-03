7e1ec7688481dd78a66fc32cbb3d1730f98a8277
chacha <chayeonggeon@gmail.com>
2026-04-03 23:56:59 +0900

    [#79] Chore: 식품 등록 페이지 테마에 따른 placeholder 색 지정

--- src/features/food-add/components/FoodNameInput.tsx
+++ src/features/food-add/components/FoodNameInput.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Control, Controller } from "react-hook-form";
-import { Input, Text, YStack, styled } from "tamagui";
+import { Input, Text, YStack, styled, useThemeName } from "tamagui";
 import { FoodFormValues } from "../types";
 
 const LabelText = styled(Text, {
@@ -14,36 +14,42 @@ export const FoodNameInput = ({
   control,
 }: {
   control: Control<FoodFormValues>;
-}) => (
-  <Controller
-    control={control}
-    name="name"
-    rules={{
-      required: "식품명을 입력해주세요.",
-      validate: (value) => value?.trim().length > 0 || "식품명을 입력해주세요.",
-      maxLength: { value: 20, message: "최대 20자까지 입력할 수 있습니다." },
-    }}
-    render={({ field: { onChange, value }, fieldState: { error } }) => (
-      <YStack>
-        <LabelText>식품명</LabelText>
-        {error && (
-          <Text color="$warning" fontSize="$3" fontWeight="600" mb="$2">
-            {error.message}
-          </Text>
-        )}
-        <Input
-          h={50}
-          value={value}
-          onChangeText={onChange}
-          placeholder="식품 이름을 입력하세요"
-          backgroundColor="$gray3"
-          fontSize="$3"
-          fontFamily="$baemin"
-          fontWeight="400"
-          br="$4"
-          bw={0}
-        />
-      </YStack>
-    )}
-  />
-);
+}) => {
+  const themeName = useThemeName();
+
+  return (
+    <Controller
+      control={control}
+      name="name"
+      rules={{
+        required: "식품명을 입력해주세요.",
+        validate: (value) =>
+          value?.trim().length > 0 || "식품명을 입력해주세요.",
+        maxLength: { value: 20, message: "최대 20자까지 입력할 수 있습니다." },
+      }}
+      render={({ field: { onChange, value }, fieldState: { error } }) => (
+        <YStack>
+          <LabelText>식품명</LabelText>
+          {error && (
+            <Text color="$warning" fontSize="$3" fontWeight="600" mb="$2">
+              {error.message}
+            </Text>
+          )}
+          <Input
+            h={50}
+            value={value}
+            onChangeText={onChange}
+            placeholder="식품 이름을 입력하세요"
+            placeholderTextColor={themeName === "dark" ? "$gray10" : "$gray5"}
+            backgroundColor="$gray3"
+            fontSize="$3"
+            fontFamily="$baemin"
+            fontWeight="400"
+            br="$4"
+            bw={0}
+          />
+        </YStack>
+      )}
+    />
+  );
+};
