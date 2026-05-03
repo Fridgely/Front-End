3a7b5c45faa4871998edb8781b299fb38308251f
chacha <chayeonggeon@gmail.com>
2026-05-03 15:39:20 +0900

    [#131] Chore: 알림설정 페이지 뒤로가기 라우팅 수정

--- src/shared/components/Header/Header.types.ts
+++ src/shared/components/Header/Header.types.ts
@@ -2,6 +2,8 @@ interface HeaderProps {
   title?: string;
   showBackButton?: boolean;
   showNotificationBell?: boolean;
+  /** 있으면 뒤로 버튼에서 `router.back()` 대신 실행 */
+  onBackPress?: () => void;
 }
 
 export { HeaderProps };
